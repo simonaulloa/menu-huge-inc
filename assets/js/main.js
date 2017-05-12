80299da5efc119945ea09f5209ba89aad3efa5c2
simona <simonaulloa@gmail.com>
2017-05-11 23:38:58 -0300

    final sin prueba

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
-document.getElementByid("toggle").addEventListener("click",fuction(event){
+document.getElementById("toggle").addEventListener("click",fuction(event){
 	event.preventDefault();
-	document.getElementByid("header").class.toggle("open");
-	document.getElementByid("body").classList.toggle("overflow-hidden");
+	document.getElementById("header").class.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");
 });
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", fuction(){
+	var currentScroll = window.pageYOffset || document.body.scrollTop;
+	if (currentScroll > lastScrollTop){
+		document.getElementById("header").classList.remove("header-in");
+		document.getElementById("header").style.opacity=0;
+	} else {
+	 	document.getElementById("header").classList.add("header-in");
+	 	document.getElementById("header").classList.add("solid");
+	 	document.getElementById("header").style.opacity = 1;
+	}
+		if(currentScroll<=3){
+	 		document.getElementById("header").classList.remove("solid");
+	 		document.getElementById("header").classList.remove("header-in");
+ 	}
+	lastScrollTop = currentScroll;
+}, false);
